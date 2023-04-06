c7d8348b98db9548e5ab572395e6a6009e1f0eb2
joinjun <dignzh@naver.com>
2023-04-06 18:31:32 +0900

    refactor : BookQueryRepository의 테스트를 spring application 테스트 ->  슬라이스 테스트로 변경

--- src/test/java/com/scaling/libraryservice/LibraryServiceApplicationTests.java
+++ src/test/java/com/scaling/libraryservice/LibraryServiceApplicationTests.java
@@ -1,23 +1,11 @@
 package com.scaling.libraryservice;
 
-import com.scaling.libraryservice.entity.Book;
-import com.scaling.libraryservice.repository.BookQueryRepository;
-import com.scaling.libraryservice.repository.BookRepository;
-import com.scaling.libraryservice.util.Tokenizer;
-import java.util.List;
-import java.util.Objects;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LibraryServiceApplicationTests {
-
-    @Autowired
-    private BookRepository bookRepo;
 
 
     @Test @DisplayName("스프링 부트 기본 시작을 테스트")
@@ -25,75 +13,7 @@ class LibraryServiceApplicationTests {
 
     }
 
-    @Nested @DisplayName("다양한 경우에 따른 같은 도서 검색")
-    class SearchSimilarBook{
 
-        @Autowired
-        private BookQueryRepository bookQueryRepo;
-
-        @Autowired
-        private Tokenizer tokenizer;
-        @Test @DisplayName("띄어쓰기 유무에 따른 검색")
-        public void search_sameTitle_different_space(){
-            /* given */
-
-            String query1 = "토비 스프링";
-            String query2 = "스프링 토비";
-
-            /* when */
-            boolean isEqualBook = isEqualBook(query1,query2);
-
-            /* then */
-            Assertions.assertTrue(isEqualBook);
-        }
-
-        @Test @DisplayName("조사 유무에 따른 검색")
-        void search_sameTitle_different_Josa(){
-
-        /*consider : 해당 테스트는 쿼리 생성을 보는 테스트이므로
-                 tokenizer도 mock으로 만들어서 단위를 분리 해야함.*/
-            /* given */
-
-            String query1 = "토비 스프링";
-            String query2 = "스프링 토비";
-
-            /* when */
-            boolean isEqualBook = isEqualBook(query1,query2);
-
-            /* then */
-            Assertions.assertTrue(isEqualBook);
-        }
-
-        @Test @DisplayName("명사 위치에 따른 검색")
-        void search_sameTitle_different_position(){
-            /* given */
-
-            String query1 = "토비 스프링";
-            String query2 = "스프링 토비";
-
-            /* when */
-            boolean isEqualBook = isEqualBook(query1,query2);
-
-            /* then */
-            Assertions.assertTrue(isEqualBook);
-        }
-
-        // 테스트에 필요한 공통 메서드
-        boolean isEqualBook(String query1,String query2){
-
-            List<String> tokens1 = tokenizer.tokenize(query1);
-            List<String> tokens2 = tokenizer.tokenize(query2);
-
-            List<Book> result1 = bookQueryRepo.findBooksByToken(tokens1);
-            List<Book> result2 = bookQueryRepo.findBooksByToken(tokens2);
-
-            Book book1 = result1.get(0);
-            Book book2 = result2.get(0);
-
-            return Objects.equals(book1.getSeqId(), book2.getSeqId());
-        }
-
-    }
 
 
 
--- src/test/java/com/scaling/libraryservice/repository/BookQueryRepositoryTest.java
+++ src/test/java/com/scaling/libraryservice/repository/BookQueryRepositoryTest.java
@@ -7,24 +7,118 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.scaling.libraryservice.entity.Book;
 import com.scaling.libraryservice.entity.QBook;
+import com.scaling.libraryservice.util.Tokenizer;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import(BookQueryRepository.class)
 class BookQueryRepositoryTest {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    @Autowired
+    private BookQueryRepository bookQueryRepo;
+
+    private Tokenizer tokenizer;
+
+    @BeforeEach
+    public void setUp() {
+
+        tokenizer = new Tokenizer(new Komoran(DEFAULT_MODEL.FULL));
+    }
+
+    @Nested
+    @DisplayName("다양한 경우에 따른 같은 도서 검색")
+    class SearchSimilarBook {
+
+        @Test
+        @DisplayName("띄어쓰기 유무에 따른 검색")
+        public void search_sameTitle_different_space() {
+            /* given */
+
+            String query1 = "토비 스프링";
+            String query2 = "스프링 토비";
+
+            /* when */
+            boolean isEqualBook = isEqualBook(query1, query2);
+
+            /* then */
+            Assertions.assertTrue(isEqualBook);
+        }
+
+        @Test
+        @DisplayName("조사 유무에 따른 검색")
+        void search_sameTitle_different_Josa() {
+
+        /*consider : 해당 테스트는 쿼리 생성을 보는 테스트이므로
+                 tokenizer도 mock으로 만들어서 단위를 분리 해야함.*/
+            /* given */
+
+            String query1 = "토비 스프링";
+            String query2 = "스프링 토비";
+
+            /* when */
+            boolean isEqualBook = isEqualBook(query1, query2);
+
+            /* then */
+            Assertions.assertTrue(isEqualBook);
+        }
+
+        @Test
+        @DisplayName("명사 위치에 따른 검색")
+        void search_sameTitle_different_position() {
+            /* given */
+
+            String query1 = "토비 스프링";
+            String query2 = "스프링 토비";
+
+            /* when */
+            boolean isEqualBook = isEqualBook(query1, query2);
+
+            /* then */
+            Assertions.assertTrue(isEqualBook);
+        }
+
+        // 테스트에 필요한 공통 메서드
+        boolean isEqualBook(String query1, String query2) {
+
+            List<String> tokens1 = tokenizer.tokenize(query1);
+            List<String> tokens2 = tokenizer.tokenize(query2);
+
+            List<Book> result1 = bookQueryRepo.findBooksByToken(tokens1);
+            List<Book> result2 = bookQueryRepo.findBooksByToken(tokens2);
+
+            Book book1 = result1.get(0);
+            Book book2 = result2.get(0);
+
+            return Objects.equals(book1.getSeqId(), book2.getSeqId());
+        }
+
+    }
 
     @Test
     void queryDsl_factory_query() {
 
         /*String query = "select * from books where TITLE_NM like '%자바%' and TITLE_NM like '%정석%'";*/
 
-        List<String> tokens = List.of("자바","정석");
+        List<String> tokens = List.of("자바", "정석");
 
         JPAQueryFactory qf = new JPAQueryFactory(entityManager);
         JPAQuery<Book> result = qf.selectFrom(QBook.book)
@@ -32,9 +126,9 @@ class BookQueryRepositoryTest {
 
         JPAQuery<Book> query1 = qf.selectFrom(QBook.book);
 
-        for(String s : tokens){
+        for (String s : tokens) {
 
-            String str = "%"+s+"%";
+            String str = "%" + s + "%";
             BooleanExpression expression = QBook.book.title.like(str);
 
             query1.where(expression);
@@ -45,7 +139,6 @@ class BookQueryRepositoryTest {
         /*result.fetch().forEach(System.out::println);*/
         /*query1.fetch().forEach(System.out::println);*/
     }
-
 
 
 }
