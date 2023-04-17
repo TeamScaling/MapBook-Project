0c82241b9aa2ece3a3506f928760520bf6d1679f
mac <ryjun91@github.com>
2023-04-17 14:03:54 +0900

    Feature/search_title in english

--- src/main/java/com/scaling/libraryservice/repository/BookRepository.java
+++ src/main/java/com/scaling/libraryservice/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.scaling.libraryservice.repository;
 
 import com.scaling.libraryservice.entity.Book;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,17 +12,24 @@ import org.springframework.stereotype.Repository;
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
     // 제목검색 FULLTEXT 서치 이용
-    @Query(value = "SELECT * FROM books WHERE MATCH(TITLE_NM) AGAINST (:query IN BOOLEAN MODE)", nativeQuery = true)
-    List<Book> findBooksByTitlePage(@Param("query") String query);
+//    @Query(value = "SELECT * FROM books WHERE MATCH(TITLE_NM) AGAINST (:query IN BOOLEAN MODE)", nativeQuery = true)
+//    List<Book> findBooksByTitlePage(@Param("query") String query);
 
     // 작가검색 FULLTEXT 서치 이용 + 페이징
     @Query(value = "SELECT * FROM books WHERE MATCH(AUTHR_NM) AGAINST (:query IN BOOLEAN MODE)", nativeQuery = true)
     Page<Book> findBooksByAuthor(@Param("query") String query, Pageable pageable);
 
     // 제목검색 FULLTEXT 서치 이용 + 페이징
-    @Query(value = "SELECT * FROM books WHERE MATCH(TITLE_NM) AGAINST (:query IN BOOLEAN MODE)",
-        countQuery = "SELECT COUNT(*) FROM books WHERE MATCH(TITLE_NM) AGAINST (:query IN BOOLEAN MODE)",nativeQuery = true)
-    Page<Book> findBooksByTitlePage(@Param("query") String query, Pageable pageable);
+//    @Query(value = "SELECT * FROM library_service.books WHERE MATCH(TITLE_NM) AGAINST (:query IN BOOLEAN MODE)",
+//        countQuery = "SELECT COUNT(*) FROM books WHERE MATCH(TITLE_NM) AGAINST (:query)",nativeQuery = true)
+//    Page<Book> findBooksByTitlePage(@Param("query") String query, Pageable pageable);
 
-    }
+    @Query(value = "SELECT * FROM books WHERE MATCH(ENG_TITILE_NM) AGAINST (:query IN BOOLEAN MODE)",
+        countQuery = "SELECT COUNT(*) FROM books WHERE MATCH(TITLE_NM) AGAINST (:query)",nativeQuery = true)
+    Page<Book> findBooksByEngilshTitlePage(String query, Pageable pageable);
+
+    @Query(value = "SELECT * FROM library_service.books WHERE MATCH(TITLE_NM) AGAINST (:query)",
+        countQuery = "SELECT COUNT(*) FROM books WHERE MATCH(TITLE_NM) AGAINST (:query)",nativeQuery = true)
+    Page<Book> findBooksByTitlePage(@Param("query") String query, Pageable pageable);
+}
 
