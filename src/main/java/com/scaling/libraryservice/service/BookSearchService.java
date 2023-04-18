a0057163c6bceabca1795da1a86272f1d26f139d
sarahLee <zbd0329@gmail.com>
2023-04-18 13:49:53 +0900

    refactoring : BookService 코드 리팩토링. 동작별로 코드단위로 나누어 리팩토링함.

--- src/main/java/com/scaling/libraryservice/service/BookSearchService.java
+++ src/main/java/com/scaling/libraryservice/service/BookSearchService.java
@@ -27,19 +27,15 @@ public class BookSearchService {
     //작가 검색 FULLTEXT + 페이징
     @Timer
     public RespBooksDto searchByAuthor(String author, int page, int size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
+        Pageable pageable = createPageable(page, size);
 
         String query = splitTarget(author);
 
         Page<Book> books = bookRepository.findBooksByAuthor(query, pageable);
 
-        List<BookDto> document = books.getContent()
-            .stream()
-            .map(BookDto::new)
-            .collect(Collectors.toList());
+        List<BookDto> document = convertToBookDtoList(books);
 
-        MetaDto meta =
-            new MetaDto(books.getTotalPages(), books.getTotalElements(), page, size);
+        MetaDto meta = createMetaDto(books, page, size);
 
         return new RespBooksDto(meta, document);
     }
@@ -48,10 +44,10 @@ public class BookSearchService {
     //제목 검색 FULLTEXT + 페이징
     @Timer
     public RespBooksDto searchByTitle(String title, int page, int size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
+        Pageable pageable = createPageable(page, size);
 
-        //기본 검색
         String query = splitTarget(title);
+
         Page<Book> books = bookRepository.findBooksByTitleNormal(query, pageable);
 
         // 검색 결과가 없는 경우, 다른 방법으로 동적으로 검색결과 변경
@@ -59,17 +55,17 @@ public class BookSearchService {
             books = bookRepository.findBooksByTitleFlexible(query, pageable);
         }
 
-        List<BookDto> documents = books.getContent()
-            .stream()
-            .map(BookDto::new)
-            .collect(Collectors.toList());
+        List<BookDto> document = convertToBookDtoList(books);
 
-        MetaDto meta =
-            new MetaDto(books.getTotalPages(), books.getTotalElements(), page, size);
+        MetaDto meta = createMetaDto(books, page, size);
 
-        return new RespBooksDto(meta, documents);
+        return new RespBooksDto(meta, document);
     }
 
+    // pageable 객체에 값 전달
+    public Pageable createPageable(int page, int size) {
+        return PageRequest.of(page - 1, size);
+    }
 
     // 띄어쓰기 전처리
     private String splitTarget(String target) {
@@ -77,6 +73,24 @@ public class BookSearchService {
             .map(name -> "+" + name)
             .collect(Collectors.joining(" "));
     }
+
+    //dto 리스트에서 참조변수에 값 전달
+    private List<BookDto> convertToBookDtoList(Page<Book> books) {
+        return books.getContent()
+            .stream()
+            .map(BookDto::new)
+            .collect(Collectors.toList());
+    }
+
+    // MetaDto 에 값 넣기
+    private MetaDto createMetaDto(Page<Book> books, int page, int size) {
+        return new MetaDto(books.getTotalPages(), books.getTotalElements(), page, size);
+    }
+
+
+
+
+
 
 }
 
