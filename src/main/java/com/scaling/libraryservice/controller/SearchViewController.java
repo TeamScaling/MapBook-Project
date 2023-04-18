320055942175c2a46627852b9d276cedb969ffa4
sarahLee <zbd0329@gmail.com>
2023-04-18 16:30:58 +0900

    feature : 백엔드 코드에 맞춰 프런트 작업. target에 맞춰 검색할 수 있도록 코드 수정.

--- src/main/java/com/scaling/libraryservice/controller/SearchViewController.java
+++ src/main/java/com/scaling/libraryservice/controller/SearchViewController.java
@@ -24,16 +24,14 @@ public class SearchViewController {
     @GetMapping("/books/search")
     public String SearchBook(@RequestParam(value = "query", required = false) String query,
         @RequestParam(value = "page", defaultValue = "1") int page,
-        @RequestParam(value = "size", defaultValue = "10") int size, ModelMap model) {
+        @RequestParam(value = "size", defaultValue = "10") int size,
+        @RequestParam(value = "target", defaultValue = "title") String target, ModelMap model) {
 
         if (!query.isEmpty()) {
-
-            RespBooksDto searchResult = searchService.searchByTitle(query, page, size);
-
+            RespBooksDto searchResult = searchService.searchBooksFlexible(query, page, size, target);
             model.put("searchResult", searchResult);
             model.put("totalPages", searchResult.getMeta().getTotalPages());
             model.put("size", searchResult.getMeta().getTotalElements());
-
         }
         //fixme : query가 empty일 때, 사용자가 공백 검색을 못하게 alert를 띄운다.
 
