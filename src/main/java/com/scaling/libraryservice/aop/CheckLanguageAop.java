7314aba5a7c5a980c65dae8c99ccdc3d4a2d2cd7
mac <ryjun91@github.com>
2023-04-21 01:44:59 +0900

    Feature/Check_Language

---
+++ src/main/java/com/scaling/libraryservice/aop/CheckLanguageAop.java
@@ -0,0 +1,42 @@
+package com.scaling.libraryservice.aop;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Log4j2
+public class CheckLanguageAop {
+
+	@Pointcut("@annotation(CheckLanguage)")
+	private void enableCheck() {
+	}
+
+	@Around("enableCheck()")
+	public Object checkLanguage(ProceedingJoinPoint pjp) throws Throwable
+	{
+
+		Object result = pjp.proceed();
+
+		Object[] args = pjp.getArgs();
+
+		String query = (String) args[0];
+		if (isEnglish(query)) {
+			log.info("[CheckLanguage] " + query + "은(는) 영어입니다.");
+		} else {
+			log.info("[CheckLanguage] " + query  +  "(은)는 한글입니다.");
+		}
+
+		return result;
+	}
+
+	public static boolean isEnglish(String input){
+		String pattern = "^[a-zA-Z\\s+]+$";
+		return input.matches(pattern);
+	}
+
+}
