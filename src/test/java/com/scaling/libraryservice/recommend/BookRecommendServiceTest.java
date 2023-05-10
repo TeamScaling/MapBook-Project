8eb99fce8c9b04db22e435e0b73711945b4c5421
mac <ryjun91@github.com>
2023-05-10 21:09:03 +0900

    feature : make BookRecommendServiceTest

---
+++ src/test/java/com/scaling/libraryservice/recommend/BookRecommendServiceTest.java
@@ -0,0 +1,27 @@
+package com.scaling.libraryservice.recommend;
+
+import com.scaling.libraryservice.recommend.service.RecommendService;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookRecommendServiceTest {
+
+		@Autowired
+		private RecommendService recommendService;
+
+		@Test
+		@DisplayName("추천 도서 검색")
+		void test_recommend_book(){
+			String title = "고양이 탐구생활 (개정판) : 고양이이에 관한 잡다한 지식 사전 - 고양이에 관한 잡다한 지식 사전";
+
+			List<String> recommendBook = recommendService.getRecommendBook(title);
+
+			System.out.println(recommendBook.toString());
+
+		}
+
+}
