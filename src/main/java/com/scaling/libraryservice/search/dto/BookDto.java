9455d0dfc69095f41e3a52e55cb0361a5b4e2521
joinjun <dignzh@naver.com>
2023-05-24 16:07:06 +0900

    fix : 대체 이미지 처리 타임리프 로직 오류. 대체 이미지 로드 방식 S3로 변경,      Book img에 대한 서버 처리에서 DB에서 null인 경우 [내용없음] 처리하던 로직 삭제. 프론트에 empty로 반환

--- src/main/java/com/scaling/libraryservice/search/dto/BookDto.java
+++ src/main/java/com/scaling/libraryservice/search/dto/BookDto.java
@@ -36,12 +36,6 @@ public class BookDto {
         this.author = book.getAuthor();
         this.isbn = book.getIsbn();
         this.bookImg =book.getBookImg();
-
-        if (book.getBookImg().isEmpty()) {
-            this.bookImg = "[내용 없음]";
-        } else {
-            this.bookImg = book.getBookImg();
-        }
     }
 
     public BookDto(BookDto bookDto) {
