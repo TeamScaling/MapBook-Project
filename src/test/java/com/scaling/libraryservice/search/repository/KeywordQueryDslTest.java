7b756881fde43ee48e105463421ad7be4dbd653f
Ryan J <107255371+InjunJo@users.noreply.github.com>
2023-07-26 22:21:37 +0900

    fix : KeywordQueryDsl에서 booleanBuilder를 통해 or 절로 찾는 로직 대신에 in 절로 변경

--- src/test/java/com/scaling/libraryservice/search/repository/KeywordQueryDslTest.java
+++ src/test/java/com/scaling/libraryservice/search/repository/KeywordQueryDslTest.java
@@ -1,8 +1,8 @@
 package com.scaling.libraryservice.search.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
