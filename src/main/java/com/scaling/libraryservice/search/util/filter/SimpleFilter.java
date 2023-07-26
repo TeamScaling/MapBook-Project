ad90014bfcc27ee39e52ff4a19656fc443e85f55
Ryan J <107255371+InjunJo@users.noreply.github.com>
2023-07-27 03:28:18 +0900

    feature : 영어 단어를 한글로 잘못 검색한 경우 오타 수정 기능 구현.

--- src/main/java/com/scaling/libraryservice/search/util/filter/SimpleFilter.java
+++ src/main/java/com/scaling/libraryservice/search/util/filter/SimpleFilter.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Component;
 public class SimpleFilter extends AbstractTileFilter implements TitleFilter {
 
     private final TitleFilter nextFilter;
+    private static final String ALLOWED_CHARS_REGEX = "[^a-zA-Z0-9가-힣ㅏ-ㅣㄱ-ㅎㅜ-ㅠ\\s]";
 
     public SimpleFilter(TitleFilter nextFilter) {
         this.nextFilter = nextFilter;
     }
-
-
 
     @Override
     public String filtering(String query) {
@@ -26,6 +25,6 @@ public class SimpleFilter extends AbstractTileFilter implements TitleFilter {
 
     // 특수문자를 제거 한다.
     private String removeSpecialChar(String query) {
-        return query.replaceAll("[^a-zA-Z0-9가-힣\\s]", "");
+        return query.replaceAll(ALLOWED_CHARS_REGEX, "");
     }
 }
